6b0b3ae5f4d2a83a886848b9822c7b355101c017
RoeserHe825 <RoeserHe825@northwested.org>
2025-02-21 13:31:39 -0500

    Update config.py

--- pygame-template2/config.py
+++ pygame-template2/config.py
@@ -8,7 +8,10 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-
+YELLOW = (255, 255, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+CYAN = (0, 255, 255)
 # Game window dimensions 
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
